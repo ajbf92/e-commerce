bf9249a0b3640f26f387a62ab74fe73e939cf43b
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-23 23:31:33 -0500

    define columns for product class/model

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { Category } = require('.');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +11,42 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // define an id column
+    id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true,
+    },
+    // define a username column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {isDecimal: true}
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {isNumeric: true}
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id',
+        // ?deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    }
+    
   },
   {
     sequelize,
